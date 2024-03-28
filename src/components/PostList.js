d02591a8136863820687d9d811a9fbe8ddc1fb08
Musharraf188 <106909179+Musharraf188@users.noreply.github.com>
2024-03-28 15:59:58 +0530

    Modal-created

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 
 export default function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAthor, setEnteredAthor] = useState("");
   function changeBodyHandler(event) {
@@ -12,16 +14,26 @@ export default function PostList() {
   function changeAuthorHandler(event) {
     setEnteredAthor(event.target.value);
   }
+  function handleModel() {
+    setModalIsVisible(false);
+  }
+  let modalContent;
+  if(modalIsVisible){
+    modalContent =   <Modal onClose={handleModel}>
+    <NewPost
+      onBodyChange={changeBodyHandler}
+      onAuthorChange={changeAuthorHandler}
+    />
+    </Modal>
+  }
   return (
     <>
-      <NewPost
-        onBodyChange={changeBodyHandler}
-        onAuthorChange={changeAuthorHandler}
-      />
+    {modalContent}
+  
       <ul className={classes.posts}>
         <Post author={enteredAthor} body={enteredBody} />
         <Post author='Musharraf' body='React js is Awesome!' />
       </ul>
     </>
   );
-}
+  }
